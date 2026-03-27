0bc4a93596984864b6251ee4dd9c68a3a17bee37
Clementterme <clement.terme.simplon@gmail.com>
2026-03-27 15:15:21 +0100

    Ajout classe Task.java

--- src/main/java/com/example/task/Task.java
+++ src/main/java/com/example/task/Task.java
@@ -1,4 +1,36 @@
 package com.example.task;
 
+import java.util.UUID;
+
 public class Task {
+
+    private final String id;
+    private String description;
+    private String etat;
+
+    public Task(String description) {
+        this.id = UUID.randomUUID().toString();
+        this.description = description;
+        this.etat = "en cours";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void terminer() {
+        this.etat = "terminé";
+    }
 }
