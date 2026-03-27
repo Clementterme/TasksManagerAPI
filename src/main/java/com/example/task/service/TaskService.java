72ad263a3af3b68e5a80207eb12513919c3a4c74
Clementterme <clement.terme.simplon@gmail.com>
2026-03-27 16:38:58 +0100

    Classe TaskService à jour

--- src/main/java/com/example/task/service/TaskService.java
+++ src/main/java/com/example/task/service/TaskService.java
@@ -1,9 +1,32 @@
 package com.example.task.service;
 
+import com.example.task.Task;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TaskService {
-    /// TO BE IMPLEMENTED
-    /// Handle and manage a list of Task objects
+
+    private List<Task> tasks = new ArrayList<>();
+
+    public void ajouterTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void supprimerTask(String id) {
+        tasks.removeIf(task -> task.getId().equals(id));
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void terminerTask(String id) {
+        for (Task task : tasks) {
+            if (task.getId().equals(id)) {
+                task.terminer();
+            }
+        }
+    }
 }
