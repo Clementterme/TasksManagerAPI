7f8e17d3bd6303ee164ecd823ff14f0313b32d3b
Clementterme <clement.terme.simplon@gmail.com>
2026-06-08 12:01:39 +0200

    Ajout de la route /tasks

--- src/main/java/com/example/task/controller/TaskController.java
+++ src/main/java/com/example/task/controller/TaskController.java
@@ -1,10 +1,13 @@
 package com.example.task.controller;
 
+import com.example.task.Task;
 import com.example.task.service.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -18,5 +21,10 @@ public class TaskController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Welcome to the Task Manager API!");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getTasks() {
+        return ResponseEntity.ok(taskService.getTasks());
     }
 }
