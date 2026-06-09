a4be648c6ee7b22790ce7f59ebb180d49a6ad215
Clementterme <clement.terme.simplon@gmail.com>
2026-06-09 11:07:26 +0200

    Ajout tests unitaires

--- src/test/java/com/example/task/controller/TaskControllerUnitTests.java
+++ src/test/java/com/example/task/controller/TaskControllerUnitTests.java
@@ -1,15 +1,18 @@
 package com.example.task.controller;
 
+import com.example.task.Task;
 import com.example.task.service.TaskService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TaskController.class)
 public class TaskControllerUnitTests {
@@ -21,8 +24,33 @@ public class TaskControllerUnitTests {
 
     @Test
     void hello_should_return_message() throws Exception {
-        mockMvc.perform(get("/tasks/hello"))
+
+        Task task1 = new Task("Faire les courses");
+        Task task2 = new Task("Apprendre Spring Boot");
+
+        Mockito.when(taskService.getTasks())
+                .thenReturn(List.of(task1, task2));
+
+        mockMvc.perform(get("/tasks"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Welcome to the Task Manager API!"));
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].description").value("Faire les courses"))
+                .andExpect(jsonPath("$[1].description").value("Apprendre Spring Boot"));
+
+//        mockMvc.perform(get("/tasks/hello"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("Welcome to the Task Manager API!"));
     }
+
+    @Test
+    void shouldReturnEmptyListWhenNoTaskExists() throws Exception {
+
+        Mockito.when(taskService.getTasks())
+                .thenReturn(List.of());
+
+        mockMvc.perform(get("/tasks"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
 }
